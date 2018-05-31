fcae4d8bf9380e42350b5ce057902bb8c8c248c9
Stan Janssen <mail@janssen.io>
2018-05-31 16:44:54 +0200

    single class that analyzes text for triggers

---
+++ lib/recommendation_bot/trigger_analyzer.rb
@@ -0,0 +1,17 @@
+module RecommendationBot
+  # This class should only combine other analyzers
+  class TriggerAnalyzer
+    def initialize(list_analyzer, username_analyzer)
+      @list_analyzer = list_analyzer
+      @username_analyzer = username_analyzer
+    end
+
+    def contains_trigger?(text)
+      lowercase_text = text.downcase
+      whitelisted = @list_analyzer.contains_trigger? lowercase_text
+      contains_username = @username_analyzer.contains_trigger? lowercase_text
+
+      whitelisted || contains_username
+    end
+  end
+end
---
+++ spec/trigger_analyzer_spec.rb
@@ -0,0 +1,43 @@
+include RecommendationBot
+
+describe TriggerAnalyzer do
+  let(:whitelist) { ['lorem', 'ipsum', 'dolor', 'sit'] }
+  let(:blacklist) { ['aap', 'noot', 'mies'] }
+  let(:username) { '/u/test_bot' }
+
+  subject do
+    list = ListAnalyzer.new(whitelist, blacklist)
+    name = ListAnalyzer.new([username], [])
+    TriggerAnalyzer.new(list, name)
+  end
+
+  it('should return true if a word is whitelisted') do
+    message = 'Dummy text usually starts with lorem.'
+    result = subject.contains_trigger? message
+    expect(result).to be true
+  end
+
+  it('should return false if a word is NOT whitelisted') do
+    message = 'Dummy text usually starts with a capital letter.'
+    result = subject.contains_trigger? message
+    expect(result).to be false
+  end
+
+  it('should return false if a word is blacklisted') do
+    message = 'Dummy text usually starts with "lorem" or "aap".'
+    result = subject.contains_trigger? message
+    expect(result).to be false
+  end
+
+  it('should ignore case') do
+    message = 'Dummy text usually starts with "lorem" or "AAP".'
+    result = subject.contains_trigger? message
+    expect(result).to be false
+  end
+
+  it('should return true if the username is mentioned') do
+    message = "aap noot #{username}"
+    result = subject.contains_trigger? message
+    expect(result).to be true
+  end
+end
